f472a0dff5c3f8497c89fede7d924de344d6f246
xanderjohnston <alex.johnston@telegraph.co.uk>
2019-10-25 15:54:48 +0100

    initial API call

---
+++ src/adapters/API.js
@@ -0,0 +1,11 @@
+const CORS_ANYWHERE = "https://cors-anywhere.herokuapp.com/"
+const BASE_URL = `${CORS_ANYWHERE}https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true`
+
+const getPictures = async () => {
+  const res = await fetch(BASE_URL)
+  return res.json(res)
+}
+
+export default {
+  getPictures
+}
