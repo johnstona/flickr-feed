ddaf55c6515a4c2efcf79c12772bee61955136cc
xanderjohnston <alex.johnston@telegraph.co.uk>
2019-10-25 15:55:02 +0100

    API to change state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import API from '../src/adapters/API'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [pictures, setPictures] = useState([])
+
+  useEffect(() => {
+    API.getPictures()
+      .then(resp => setPictures(resp.items))
+  }, [])
+
 }
 
 export default App;
