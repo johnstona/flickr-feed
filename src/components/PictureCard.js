0e3c5b1c08c73c6a2229404f01ae5d2c0709ef9f
Alex Johnston <alexjohnston@ymail.com>
2019-10-28 16:46:01 +0000

    tags dropdown

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,19 +1,31 @@
 import React from 'react'
 
-const PictureCard = ({media, link, title, author, author_id, description, tags}) => {
+const PictureCard = ({media, link, title, author, author_id, description, tags, changeTag}) => {
 
   const address = "www.flickr.com/people/"
   const photo = media.m
   const name = author.split('"')
-  console.log(name)
+
+  const handleChange = (e) => {
+    changeTag(e.target.value)
+  }
 
   return <div className='picture-card'>
+          <div className='image-container'>
           <img src={photo} className={'image'}/>
+          </div>
           <div className="title-and-author">
             <a href={link}>{title}</a> by <a href={`${address}${author_id}`}>{name[1]} </a>
           </div>
           {/* <div className="description" dangerouslySetInnerHTML={{__html: `${description}`}}>
           </div> */}
+          {tags && <select name="tags" className={"tags-dropdown"} onChange={handleChange}>
+            <option value="">Tags:</option>
+            {tags.split(" ").map(tag => {
+              return <option value={tag}>{tag}</option>
+            })}
+            </select>
+          }
         </div>
 }
 
